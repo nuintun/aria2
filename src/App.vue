ad1ead04b58f6593e5c7bd75e3c997b298d207c8
nuintun <nuintun@qq.com>
2017-06-17 13:42:40 +0800

    update files

--- src/App.vue
+++ src/App.vue
@@ -46,24 +46,26 @@
           <v-list-item>
             <v-list-tile ripple>
               <v-list-tile-title @click="auth = !auth">连接设置</v-list-tile-title>
-              <v-dialog v-model="auth" scrollable :width="600">
+              <v-dialog persistent v-model="auth" scrollable :width="600">
                 <v-card>
                   <v-card-row>
-                    <v-card-title>Aria2设置</v-card-title>
+                    <v-card-title>Aria2 设置</v-card-title>
                   </v-card-row>
+                  <v-divider light></v-divider>
                   <v-card-row>
                     <v-card-text>
-                      <v-text-field label="服务器地址" required dark autofocus></v-text-field>
-                      <v-text-field label="服务器端口" required hint="Aria2默认端口：6800" dark></v-text-field>
-                      <v-text-field label="RPC路径" required hint="Aria2默认RPC：/jsonrpc" dark></v-text-field>
-                      <v-text-field label="服务器密码" type="password" dark></v-text-field>
-                      <v-switch label="SSL/TLS 加密" primary v-model="ssl" dark></v-switch>
-                      <small>带*为必填项</small>
+                      <v-text-field label="服务器地址" required dark></v-text-field>
+                      <v-text-field label="服务器端口" required hint="Aria2 默认端口：6800" dark></v-text-field>
+                      <v-text-field label="RPC路径" required hint="Aria2 默认 RPC：/jsonrpc" dark></v-text-field>
+                      <v-text-field label="服务器密码" type="password" class="mb-0" dark></v-text-field>
+                      <v-switch primary hide-details label="SSL/TLS 加密" v-model="ssl" class="mt-0" dark></v-switch>
+                      <small class="right">提示：带 * 为必填项</small>
                     </v-card-text>
                   </v-card-row>
+                  <v-divider light></v-divider>
                   <v-card-row actions>
-                    <v-btn light info @click.native="dialog = false">保存</v-btn>
-                    <v-btn light class="ml-2 grey darken-1" @click.native="dialog = false">取消</v-btn>
+                    <v-btn light info @click.native="auth = false">保存</v-btn>
+                    <v-btn light class="ml-2 grey darken-1" @click.native="auth = false">取消</v-btn>
                   </v-card-row>
                 </v-card>
               </v-dialog>
@@ -91,17 +93,16 @@ export default {
     return {
       url: '127.0.0.1',
       port: 6800,
-      auth: false,
-      configure: false,
-      drawer: true,
       ssl: false,
+      mini: false,
+      auth: false,
+      drawer: true,
+      configure: false,
       items: [
         { title: '正在下载', icon: 'archive', count: 6 },
         { title: '已完成', icon: 'done_all', count: 8 },
         { title: '已删除', icon: 'delete', count: 10 }
-      ],
-      mini: false,
-      right: null
+      ]
     }
   }
 }
