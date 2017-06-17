bccd2ea481927ca8dbfaaed2dfb7d332c17d5210
nuintun <nuintun@qq.com>
2017-06-17 11:06:24 +0800

    update files

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-title>{{ item.title }} ({{ item.count }})</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list-item>
