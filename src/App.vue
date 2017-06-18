1e8980e4b3eda150106073dee16de92490e2846d
nuintun <nuintun@qq.com>
2017-06-18 12:38:39 +0800

    updata files

--- src/App.vue
+++ src/App.vue
@@ -27,27 +27,23 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed light>
-      <v-toolbar-side-icon light v-if="!drawer" @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon light v-tooltip:bottom="{html: '显示菜单'}" v-if="!drawer" @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-logo>
         <img class="ui-logo" src="/public/logo.png" />
         <v-toolbar-title>Aria2</v-toolbar-title>
       </v-toolbar-logo>
+      <v-btn icon="icon" light v-tooltip:bottom="{html: '连接设置'}" @click.native="auth = !auth">
+        <v-icon>account_circle</v-icon>
+        <aria2-auth :auth.sync="auth"></aria2-auth>
+      </v-btn>
+      <v-btn icon="icon" light v-tooltip:bottom="{html: '全局设置'}">
+        <v-icon>settings</v-icon>
+      </v-btn>
       <v-menu bottom left>
-        <v-btn icon="icon" slot="activator" light>
+        <v-btn icon="icon" slot="activator" light v-tooltip:bottom="{html: '其它选项'}">
           <v-icon>more_vert</v-icon>
         </v-btn>
         <v-list dense class="pt-0 pb-0">
-          <v-list-item>
-            <v-list-tile ripple @click.native="auth = !auth">
-              <v-list-tile-title>连接设置</v-list-tile-title>
-              <aria2-auth :auth.sync="auth"></aria2-auth>
-            </v-list-tile>
-          </v-list-item>
-          <v-list-item>
-            <v-list-tile ripple>
-              <v-list-tile-title>全局设置</v-list-tile-title>
-            </v-list-tile>
-          </v-list-item>
           <v-list-item>
             <v-list-tile ripple>
               <v-list-tile-title>服务器信息</v-list-tile-title>
@@ -79,11 +75,13 @@ export default {
       drawer: true,
       configure: false,
       items: [
-        { title: '正在下载', icon: 'archive', count: 6 },
+        { title: '正在下载', icon: 'cloud_download', count: 6 },
         { title: '已完成', icon: 'done_all', count: 8 },
         { title: '已删除', icon: 'delete', count: 10 }
       ]
     }
   }
 }
+
+// archive
 </script>
