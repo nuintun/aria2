8a9ab76b91fb2732ed2bc1d75244631990a47052
nuintun <nuintun@qq.com>
2017-06-19 14:54:30 +0800

    update files

--- src/App.vue
+++ src/App.vue
@@ -58,8 +58,35 @@
       </v-menu>
     </v-toolbar>
     <main>
-      <v-container fluid>
-        <div class="title">额，不知道说啥~</div>
+      <v-container fluid class="pa-0">
+        <v-toolbar class="transparent elevation-0" dark>
+          <v-btn icon="icon" v-tooltip:bottom="{html: '全局设置'}">
+            <v-icon>pause</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-divider dark></v-divider>
+        <v-card>
+          <v-card-title v-if="false">
+            <v-btn icon="icon" v-tooltip:bottom="{html: '全局设置'}">
+              <v-icon>settings</v-icon>
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-text-field append-icon="search" label="Search" single-line hide-details v-model="table.search"></v-text-field>
+          </v-card-title>
+          <v-divider dark class="grey lighten-1"></v-divider>
+          <v-data-table dark hide-actions :headers="table.headers" :items="table.items" :search="table.search" v-model="table.selected" select-all selected-key="id">
+            <template slot="items" scope="props">
+              <td>
+                <v-checkbox primary hide-details v-model="props.selected"></v-checkbox>
+              </td>
+              <td>{{ props.item.status }}</td>
+              <td>{{ props.item.type }}</td>
+              <td>{{ props.item.name }}</td>
+              <td>{{ props.item.process }}</td>
+              <td class="text-xs-right">{{ props.item.speed }}</td>
+            </template>
+          </v-data-table>
+        </v-card>
       </v-container>
     </main>
   </v-app>
@@ -78,7 +105,65 @@ export default {
         { title: '正在下载', icon: 'cloud_download', count: 6 },
         { title: '已完成', icon: 'done_all', count: 8 },
         { title: '已删除', icon: 'delete', count: 10 }
-      ]
+      ],
+      table: {
+        search: '',
+        selected: [],
+        headers: [
+          {
+            text: '状态',
+            left: true,
+            sortable: false,
+            value: 'status'
+          },
+          { text: '类型', value: 'type', left: true },
+          { text: '名称', value: 'name', left: true },
+          { text: '进度/剩余时间', value: 'process', left: true },
+          { text: '下载速度', value: 'speed', left: false }
+        ],
+        items: [
+          {
+            id: 1,
+            status: '下载',
+            type: '',
+            name: 87,
+            process: '14%',
+            speed: '1%'
+          },
+          {
+            id: 2,
+            status: '等待',
+            type: 'Ice cream sandwich',
+            name: 237,
+            process: 9.0,
+            speed: 37
+          },
+          {
+            id: 3,
+            status: '暂停',
+            type: 'Eclair',
+            name: 262,
+            process: 16.0,
+            speed: 23
+          },
+          {
+            id: 4,
+            status: '暂停',
+            type: 'Eclair',
+            name: 262,
+            process: 16.0,
+            speed: 23
+          },
+          {
+            id: 5,
+            status: '暂停',
+            type: 'Eclair',
+            name: 262,
+            process: 16.0,
+            speed: 23
+          }
+        ]
+      }
     }
   }
 }
