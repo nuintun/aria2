72d8b53cf99dacee8c955992ff026129f8b9d1fe
nuintun <nuintun@qq.com>
2017-06-21 15:36:19 +0800

    update files

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,7 +4,8 @@ module.exports = {
   plugins: {
     // to edit target browsers: use "browserlist" field in package.json
     autoprefixer: {
-      add: true
+      add: true,
+      browsers: ['last 2 versions', 'Firefox ESR', '> 1%', 'ie >= 8', 'iOS >= 8', 'Android >= 4']
     }
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    main: './src/main.js'
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].js'
   },
   resolve: {
     alias: {
